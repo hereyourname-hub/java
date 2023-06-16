486f91addd014f87bf2190c500a9979dcc67be21
Anz <nurlanbekova@adam.edu.kg>
2023-06-16 13:37:00 +0600

    16.06.23 JAVA

--- app/src/main/java/com/example/bodyboost/HomeActivity.java
+++ app/src/main/java/com/example/bodyboost/HomeActivity.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
@@ -19,6 +21,49 @@ public class HomeActivity extends AppCompatActivity {
         String username = sharedpreferences.getString("username", "").toString();
         Toast.makeText(getApplicationContext(),"Welcom"+username,Toast.LENGTH_SHORT).show();
 
-        CardView exit = findViewById(R.id.cardWater)
+        CardView findWater = findViewById(R.id.tv_water_card);
+        findWater.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomeActivity.this,WaterActivity.class));
+            }
+        });
+
+        CardView findMedicine = findViewById(R.id.tv_medicine_card);
+        findMedicine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomeActivity.this,MedicineActivity.class));
+            }
+        });
+
+
+
+        CardView findFood = findViewById(R.id.tv_food_card);
+        findFood.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomeActivity.this,FoodActivity.class));
+            }
+        });
+
+
+
+
+
+
+        CardView findSleep = findViewById(R.id.tv_sleep_card);
+        findSleep.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomeActivity.this,SleepActivity.class));
+            }
+        });
+
+
+
+
     }
+
+
 }
---
+++ app/src/main/java/com/example/bodyboost/HomeMenuActivity.java
@@ -0,0 +1,13 @@
+package com.example.bodyboost;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
